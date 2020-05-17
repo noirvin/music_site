a1640d28b56167ecada5ce5f30e7c9acf2254127
noirvin <vin.sp97@googlemail.com>
2020-05-17 00:03:55 -0700

    artist page loads

--- music/views.py
+++ music/views.py
@@ -9,14 +9,14 @@ def home(request):
 
 
 
-def artist(request):
+def artist(request,musician_id):
     album_list = {
         'musician': Musician.objects.get(id=musician_id),
         'albums': Album.objects.filter(id=musician_id),
   }
-    return render(request, 'home.html', album_list)
+    return render(request, 'artist.html', album_list)
 
-def album(request):
+def album(request,album_id):
     song_list = {
         'album': Album.objects.get(id=album_id),
         'songs': Song.objects.filter(album=album_id)
